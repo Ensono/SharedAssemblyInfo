152a10575705106a68ed3da8f5d75bd301494c9f
Richard Slater <richard.slater@amido.com>
2015-05-20 21:29:34 +0100

    Updating to Support Multiple Tiers

--- Scripts/Initialize-SharedAssemblyInfo.ps1
+++ Scripts/Initialize-SharedAssemblyInfo.ps1
@@ -1,3 +1,54 @@
+﻿function Set-SharedAssemblyInfo {
+  Param(
+    [parameter(Mandatory=$true)]
+    $SharedAssemblyInfoFile,    
+    [parameter(Mandatory=$true)]
+    $Company = "Amido Limited",
+    [parameter(Mandatory=$true)]
+    $Product = "Product",
+    [parameter(Mandatory=$true)]
+    $Year = (Get-Date).Year,
+    [parameter(Mandatory=$true)]
+    $Trademark = ""
+        )
+
+  process {
+    $template = "using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration(`"debug`")]
+[assembly: AssemblyCompany(`"$Company`")]
+[assembly: AssemblyProduct(`"$Product`")]
+[assembly: AssemblyCopyright(`"©$Year $Company, All Rights Reserved`")]
+[assembly: AssemblyTrademark(`"$Trademark`")]
+[assembly: AssemblyCulture(`"`")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion(`"1.0.*`")]
+[assembly: AssemblyVersion(`"1.0.0.0`")]
+[assembly: AssemblyFileVersion(`"1.0.0.0`")]"
+
+  Set-Content -Path $SharedAssemblyInfoFile -Value $template -Encoding UTF8;
+  return;
+  }
+}
+
 function Initialize-SharedAssemblyInfo {
   Param(
     [Parameter(Mandatory=$False)]
@@ -20,6 +71,12 @@ function Initialize-SharedAssemblyInfo {
     Write-Host -ForegroundColor Green "Found Solutions at $solutions";
 
     $sharedAssemblyInfo = Join-Path $solutions "SharedAssemblyInfo.cs";
+
+    if (-Not (Test-Path $sharedAssemblyInfo)) {
+      Write-Host -ForegroundColor Yellow "SharedAssemblyInfo.cs not found, supply some details and we can set it up for you...";
+      Set-SharedAssemblyInfo -SharedAssemblyInfoFile $sharedAssemblyInfo;
+    }
+
     $assemblyInfoFragment = "<Compile Include=`"Properties\AssemblyInfo.cs`" />";
     $sharedAssemblyInfoFragment = "`t`t<Compile Include=`"{0}`">" + $crlf + "`t`t`t<Link>Properties\SharedAssemblyInfo.cs</Link>" + $crlf + "`t`t</Compile>";
 
